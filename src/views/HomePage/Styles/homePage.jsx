7521b3b1225e5c104f2cd74cc0848e6006645f58
Gautier Darchen <darchen.gautier@gmail.com>
2019-03-10 16:49:28 +0100

    fix: use media query to remove the content's margins on small devices

--- src/views/HomePage/Styles/homePage.jsx
+++ src/views/HomePage/Styles/homePage.jsx
@@ -24,6 +24,9 @@ const homePageStyle = {
   },
   mainRaised: {
     margin: "-60px 30px 0px",
+    "@media (max-width: 767px)": {
+      margin: "0"
+    },
     borderRadius: "6px",
     boxShadow:
       "0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
