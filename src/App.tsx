706b3f489246882929ca13aac53e63320445c138
Gautier Darchen <darchen.gautier@gmail.com>
2024-07-05 17:55:32 +0200

    refactor(archive): redirect to new website unless `?redirect=false` is provided

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Route, Routes, useSearchParams } from 'react-router-dom';
 import Home from './pages/Home/Home';
 
 const App = () => {
+  const [searchParams] = useSearchParams();
+
+  const redirectParam = searchParams.get('redirect');
+  const mustRedirect = !redirectParam || redirectParam === 'true';
+
+  useEffect(() => {
+    if (!mustRedirect) {
+      alert('This website is an old version.\nPlease check https://gautierdarchen.fr instead.');
+      return;
+    }
+    alert('This website is an old version.\nYou are going to be redirected to the new one.');
+    window.location.href = 'https://gautierdarchen.fr';
+  }, [mustRedirect]);
+
   return (
     <div className="App">
       <Routes>
